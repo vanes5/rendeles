34810636cd8df49200bbc42d4e7eefd13a874ee6
Gecse Vanessza <vanessza.gecse@gmail.com>
2024-10-07 14:11:55 +0200

    elvileg kesz de meg valami nem jo

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,84 @@
-import { Controller, Get, Query, Render } from '@nestjs/common';
+import { Controller, Get, Query, Render, Res } from '@nestjs/common';
 import { AppService } from './app.service';
-import { query } from 'express';
+import { response } from 'express';
+
+class Felhasznalo{
+  nev: string
+  orszag: string
+  irszam: string
+  varos: string
+  uhSzam: string
+  kupon: string
+}
 
 @Controller()
 export class AppController {
+  #Felhasznalok: Felhasznalo[] = [];
   constructor(private readonly appService: AppService) {}
-
+  
   @Get()
   @Render('index')
-  getHello(@Query('nev') Nev: string, @Query('orszag') Orszag:string, @Query('iranyitonszam') IrSzam: string, @Query('varos') Varos: string, @Query('utcaHazszam') UHszam: string, @Query('kuponkod') kupon: string, @Query('bankkartyaszam') bkSzam: string, @Query('lejaratiDatum') lDatum: string, @Query('biztonsagiKod') bKod: string) {
+  getHello(@Query('nev') Nev: string, @Query('orszag') Orszag:string, @Query('iranyitoszam') IrSzam: string, @Query('varos') Varos: string, @Query('utcaHazszam') UHszam: string, @Query('kuponkod') kupon: string, @Query('bankkartyaszam') bkSzam: string, @Query('lejaratiDatum') lDatum: string, @Query('biztonsagiKod') bKod: string, @Res() response) {
 
-    if(Nev == null || Orszag == null || IrSzam == null || Varos == null || UHszam == null || kupon == null || bkSzam == null || lDatum ==null || bKod == null){
+    const Rendeles: Felhasznalo = {
+      nev: Nev,
+      orszag: Orszag,
+      irszam: IrSzam,
+      varos: Varos,
+      kupon: kupon,
+      uhSzam: UHszam
+
+    }
+
+    if(Nev == "" || Orszag == "" || IrSzam == "" || Varos == "" || UHszam == "" || bkSzam == ""|| lDatum =="" || bKod == ""){
     return {
-      a: true 
+      a:"Minden mezőt ki kell tölteni!",
+      Rendeles
     }
+    }else if(!/\d{4}/.test(IrSzam)){
+      Rendeles.irszam = ""
+      return {
+        a:"Irányítószám formátuma nem megfelelő!",
+        Rendeles
+      }
+    }
+    else if(kupon != ""){
+      if (!/^[A-Z]{2}-\d{4}$/.test(kupon)) {
+        Rendeles.kupon = "";
+        return {
+          a:"Kupon formátuma nem megfelelő!",
+          Rendeles
+        }
+      }
+    }
+    else if(!/^\d{4}-\d{4}-\d{4}-\d{4}$/.test(bkSzam)){
+      return {
+        a:"Bankkártyaszám formátuma nem megfelelő!",
+        Rendeles
+      }
+    }
+    else if(!/^(0[1-9]|1[0-2])\/([0-9]{2})$/.test(lDatum)){
+      return {
+        a:"Lejárati dátum formátuma nem megfelelő!",
+        Rendeles
+      }
+    }
+    else if(!/^\d{3}$/.test(bKod)){
+      return {
+        a:"Biztonsági kód formátuma nem megfelelő!",
+        Rendeles
+      }
+    }
+    else{
+      return response.redirect("/sikeresRendeles")
     }
 
+  }
+
+  @Get('sikeresRendeles')
+  @Render('siker')
+  rSiker(){
+    return;
   }
 
 
